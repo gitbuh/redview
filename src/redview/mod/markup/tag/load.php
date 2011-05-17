515dab431f1200a7925d80e88800199eecf9004d
Nick <no@spam.net>
2011-05-17 16:58:23 -0400

    params to load tag accept 'object' attribute in place of 'value', allowing non-string-literal params.

--- src/redview/mod/markup/tag/load.php
+++ src/redview/mod/markup/tag/load.php
@@ -9,15 +9,28 @@ class RedView_Mod_Markup_Tag_Load extends RedView_Mod_Markup_Tag {
       $file = $parser->tools->cache->findLoader(trim($this->attribs['view'], '/').'.html');
       $params = array();
       // <load> can have parameters inside: <param name="..." value="..." />
+      
+      $objectParamString = "";
+      
       if ($node->childNodes) {
         foreach ($node->childNodes as $child) if ($child->hasAttributes()) {
           $name = $child->getAttribute('name');
           $value = $child->getAttribute('value');
-          if ($name) $params[$name]=$value;
+          $object = $child->getAttribute('object');
+          if ($name) {
+            if ($object) {
+              $objectParamString .= " \$params['$name']=$object;";
+            } else {
+              $params[$name]=$value;
+            }
+          }
         }
       }
-      $_params = var_export($params, true);
-      $pi   = $dom->createProcessingInstruction('php', "\$params=$_params; require '$file';");
+      $paramString = var_export($params, true);
+      
+      
+      $pi   = $dom->createProcessingInstruction('php', 
+          "\$params=$paramString;$objectParamString require '$file';");
       $node->parentNode->replaceChild($pi, $node);
     }
   }
